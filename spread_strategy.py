d7a57b89214dae8c23ca4fe121fae1568b36940c
FilipchukAl <filipchuk.al@gmail.com>
2023-12-11 15:51:52 +0300

    11.12.2023

--- spread_strategy.py
+++ spread_strategy.py
@@ -1,6 +1,8 @@
 import time, uuid, datetime
 
 from pandas import DataFrame
+from typing import Optional, Dict, Any, Union
+import requests
 from tinkoff.invest.services import InstrumentsService
 from tinkoff.invest import (
     Client,
@@ -19,6 +21,59 @@ def is_within_time_interval(start_time, end_time):
 def is_weekday():
     now = datetime.datetime.now().weekday()
     return now >= 0 and now <= 4  # Понедельник - 0, Пятница - 4
+
+# Получаем информацию об инструментах
+def create_instruments_dataframe() -> DataFrame:
+
+    with Client(TOKEN) as client:
+
+        instruments: InstrumentsService = client.instruments
+
+        data = []
+        try:
+            for method in ['shares', 'bonds', 'etfs', 'currencies', 'futures']:
+                for item in getattr(instruments, method)().instruments:
+                    data.append({
+                        'ticker': item.ticker,
+                        'figi': item.figi,
+                        'type': method,
+                        'name': item.name,
+                        'lot_size': item.lot
+                    })
+
+        except requests.exceptions.RequestException as e:
+
+            raise Exception(f"Ошибка запроса: {e}")
+
+    return DataFrame(data)
+
+def get_instrument_info(df: DataFrame, TICKER: Optional[str] = None, FIGI: Optional[str] = None) -> Union[str, Dict[str, Any]]:
+
+    if TICKER:
+        filtered_df = df[df['ticker'] == TICKER]
+        if not filtered_df.empty:
+            return {
+                'ticker': filtered_df.iloc[0]['ticker'],
+                'figi': filtered_df.iloc[0]['figi'],
+                'name': filtered_df.iloc[0]['name'],
+                'lot_size': filtered_df.iloc[0]['lot_size']
+            }
+        else:
+            return f"Тикер {TICKER} не найден."
+
+    elif FIGI:
+        filtered_df = df[df['figi'] == FIGI]
+        if not filtered_df.empty:
+            return {
+                'ticker': filtered_df.iloc[0]['ticker'],
+                'figi': filtered_df.iloc[0]['figi'],
+                'name': filtered_df.iloc[0]['name'],
+                'lot_size': filtered_df.iloc[0]['lot_size']
+            }
+        else:
+            return f"FIGI {FIGI} не найден."
+
+    return "Необходимо указать тикер или FIGI."
 
 def user_input_token():
 
@@ -39,35 +94,48 @@ def user_input_token():
             time.sleep(0.1)
             print("----------------------------------------------")
     
-def user_input_tiker():
+def user_input_tiker(df):
 
     while True:
-        
+
         tiker_number = input("Выберите компанию:\n1 - Сбербанк\n2 - Татнефть\n3 - Ростелеком\n")
 
         # Обрабатываем выбор пользователя
         if tiker_number == '1':
+
             print("Вы выбрали Сбербанк. Запрашиваю информацию о портфеле...")
-            figi_Ob = ticker_figi('SBER')
-            figi_Pref = ticker_figi('SBERP')
-            name_Ob = ('SBER')
-            name_Pref = ('SBERP')
-            return figi_Ob, figi_Pref, name_Ob, name_Pref
+            info_Ob = get_instrument_info(df, TICKER="SBER")
+            info_Pref = get_instrument_info(df, TICKER="SBERP")
+
+            figi_Ob = info_Ob.get('figi', 'Информация не найдена')
+            figi_Pref = info_Pref.get('figi', 'Информация не найдена')
+
+            return figi_Ob, figi_Pref
+
         elif tiker_number == '2':
+
             print("Вы выбрали Татнефть. Запрашиваю информацию о портфеле...")
-            figi_Ob = ticker_figi('TATN')
-            figi_Pref = ticker_figi('TATNP')
-            name_Ob = ('TATN')
-            name_Pref = ('TATNP')
-            return figi_Ob, figi_Pref, name_Ob, name_Pref
+            info_Ob = get_instrument_info(df, TICKER="TATN")
+            info_Pref = get_instrument_info(df, TICKER="TATNP")
+
+            figi_Ob = info_Ob.get('figi', 'Информация не найдена')
+            figi_Pref = info_Pref.get('figi', 'Информация не найдена')
+
+            return figi_Ob, figi_Pref
+
         elif tiker_number == '3':
+
             print("Вы выбрали Ростелеком. Запрашиваю информацию о портфеле...")
-            figi_Ob = ticker_figi('RTKM')
-            figi_Pref = ticker_figi('RTKMP')
-            name_Ob = ('RTKM')
-            name_Pref = ('RTKMP')
-            return figi_Ob, figi_Pref, name_Ob, name_Pref
+            info_Ob = get_instrument_info(df, TICKER="RTKM")
+            info_Pref = get_instrument_info(df, TICKER="RTKMP")
+
+            figi_Ob = info_Ob.get('figi', 'Информация не найдена')
+            figi_Pref = info_Pref.get('figi', 'Информация не найдена')
+
+            return figi_Ob, figi_Pref
+
         else:
+
             print("Некорректный выбор. Пожалуйста, выберите 1, 2 или 3.")
             time.sleep(0.1)
             print("Попробуйте еще раз.")
@@ -86,7 +154,7 @@ def user_input_spread():
             print("Некорректные значения. Первое число должно быть меньше второго")
             
 # Функция показывающая кол-во обычки и префов в портфеле и состав портфеля
-def get_portfolio_info(TOKEN, name_Ob, name_Pref):
+def get_portfolio_info(TOKEN, df):
 
     with Client(TOKEN) as client:
 
@@ -109,45 +177,12 @@ def get_portfolio_info(TOKEN, name_Ob, name_Pref):
                 for security in securities_info:
                     figi = security.figi
                     balance = security.balance
-                    print(f"Акции: figi - '{figi}' {balance} штук")
-                break
-
-            except Exception as e:
-
-                print(f"Ошибка при получении информации о портфеле: {e}")
-                print("----------------------------------------------")
-                retries += 1
-                time.sleep(10)  # Подождать перед повторным запросом
-        else:
-
-            print("Не удалось получить информацию о портфеле после", MAX_RETRIES, "попыток.")
-            print("----------------------------------------------")
-
-        Securities = PositionResponse.securities
-
-        KObich = 0
-        KPrefa = 0
-
-        for security in Securities:
-            if security.figi == figi_Pref:
-                KPrefa = security.balance
-            if security.figi == figi_Ob:
-                KObich = security.balance
-
-        return KPrefa, KObich, Securities, PositionResponse
-
-# Функция показывающая только кол-во обычки и префов в портфеле
-def get_securities_info(TOKEN, name_Ob, name_Pref):
-
-    with Client(TOKEN) as client:
-
-        retries = 0
-
-        while retries < MAX_RETRIES:
-
-            try:
-
-                PositionResponse = client.operations.get_positions(account_id=Account_id)
+                    # Используем функцию get_instrument_info для получения тикера и имени по FIGI
+                    instrument_info = get_instrument_info(df, FIGI=figi)
+                    if isinstance(instrument_info, dict):
+                        print(f"Акции: Тикер: Название: '{instrument_info['name']}', '{instrument_info['ticker']}' - {balance} шт.")
+                    else:
+                        print(instrument_info)
                 break
 
             except Exception as e:
@@ -199,34 +234,6 @@ def get_last_prices(TOKEN, figi_Ob, figi_Pref):
 
         return spread, LastPrice_Ob, LastPrice_Pref
 
-# Получаем фиги из тикера
-def ticker_figi(TICKER):
-
-    with Client(TOKEN) as client:
-        instruments: InstrumentsService = client.instruments
-
-        l = []
-        for method in ['shares', 'bonds', 'etfs']: # , 'currencies', 'futures']:
-            for item in getattr(instruments, method)().instruments:
-                l.append({
-                    'ticker': item.ticker,
-                    'figi': item.figi,
-                    'type': method,
-                    'name': item.name,
-                })
- 
-        df = DataFrame(l)
-        # df.to_json()
- 
-        df = df[df['ticker'] == TICKER]
-
-        if df.empty:
-            print(f"Нет тикера {TICKER}")
-            return
- 
-        # print(df.iloc[0])
-        return(df['figi'].iloc[0])
-
 # Функция покупки/продажи
 def trade (TOKEN, Order, Account_id, KolVo, FigiStock, OrderDirection):
 
@@ -261,15 +268,14 @@ if __name__ == '__main__':
     print("---------------------------------------------")
     print('Скрипт торговли по спреду обычка/преф запущен')
     print("---------------------------------------------")
-
     TOKEN, Account_id = user_input_token()
-    figi_Ob, figi_Pref, name_Ob, name_Pref = user_input_tiker()
+    print('Запрашиваю информацию об инструментах...')
+    df = create_instruments_dataframe()
     print("---------------------------------------------")
-    print('Ваш портфель:')
-    KPrefa, KObich, Securities, PositionResponse = get_portfolio_info(TOKEN, name_Ob, name_Pref)
-    print('Из них:')
-    print(f'Обычка {name_Ob} = {KObich} шт.')
-    print(f'Префа {name_Pref} = {KPrefa} шт.')
+    figi_Ob, figi_Pref = user_input_tiker(df)
+    print("---------------------------------------------")
+    print('Портфель:')
+    KPrefa, KObich, Securities, PositionResponse = get_portfolio_info(TOKEN, df)
     print("---------------------------------------------")
     price_1, price_2 = user_input_spread()
     print("---------------------------------------------")
@@ -281,8 +287,10 @@ if __name__ == '__main__':
                 if is_weekday() and (is_within_time_interval(datetime.time(10, 0), datetime.time(18, 45)) or is_within_time_interval(datetime.time(19, 0), datetime.time(23, 59))):
 
                     spread, LastPrice_Ob, LastPrice_Pref = get_last_prices(TOKEN, figi_Ob, figi_Pref)
-                    KPrefa, KObich, Securities, PositionResponse = get_securities_info(TOKEN, name_Ob, name_Pref)
-                    print("---------------------------------------------")
+                    info_Ob = get_instrument_info(df, FIGI=figi_Ob)
+                    info_Pref = get_instrument_info(df, FIGI=figi_Pref)
+                    name_Ob = info_Ob.get('name', 'Неизвестное название')
+                    name_Pref = info_Pref.get('name', 'Неизвестное название')
 
                     # Спред меньше price_1 покупаем обычку если ничего нет, или продаем префа и покупаем обычку
                     if spread < price_1:
@@ -292,8 +300,8 @@ if __name__ == '__main__':
                         print ('Цена обычки =', LastPrice_Ob)
                         print ('Цена префов =', LastPrice_Pref)
                         print ('Дата:', datetime.datetime.now()) 
-                        print ('Наш портфель акций:', Securities)
-                        print ('У нас в портфеле денег:', int(PositionResponse.money[0].units), 'руб.')
+                        print('Портфель:')
+                        KPrefa, KObich, Securities, PositionResponse = get_portfolio_info(TOKEN, df)
 
                         KolVo_Ob = 0
                         KolVo_Pref = 0
@@ -312,24 +320,18 @@ if __name__ == '__main__':
 
                         # Если у нас в портфеле префа мы их продаем и покупаем обычку
                         if figi_Pref_found > 0:
-
+                            
+                            # Совершаем продажу
                             print('У нас в портфеле префа', KolVo_Pref, 'шт. нужно их продать и купить обычку')
-
-                            # У Сбера и Ростелекома лотность 10 шт.
-                            if name_Pref == 'SBERP' or name_Pref == 'RTKMP':
-                                KolVo_Pref = KolVo_Pref/10
-
+                            lot_size = info_Pref.get('lot_size', 'Информация о лотности не найдена')
+                            KolVo_Pref = KolVo_Pref/lot_size 
                             trade (TOKEN, str(uuid.uuid4()), Account_id, KolVo_Pref, figi_Pref, OrderDirection.ORDER_DIRECTION_SELL)
-
+                            
+                            # Получаем информацию для покупки
                             spread, LastPrice_Ob, LastPrice_Pref = get_last_prices(TOKEN, figi_Ob, figi_Pref)
                             Money =  int(PositionResponse.money[0].units)
-
-                            # У Сбера и Ростелекома лотность 10 шт.
-                            if name_Ob == 'SBER' or name_Ob == 'RTKM':
-                                KolVo = int(Money/LastPrice_Ob/10*0.98)
-                            else:
-                                KolVo = int(Money/LastPrice_Ob*0.98)
-
+                            lot_size = info_Ob.get('lot_size', 'Информация о лотности не найдена')
+                            KolVo = int(Money/LastPrice_Ob/lot_size*0.98)
                             trade (TOKEN, str(uuid.uuid4()), Account_id, KolVo, figi_Ob, OrderDirection.ORDER_DIRECTION_BUY)
 
                         if figi_Ob_found > 0:
@@ -340,29 +342,26 @@ if __name__ == '__main__':
 
                             print(f'В портфеле нет акций {name_Ob}, нужно купить обычку')
 
+                            # Получаем информацию для покупки
                             spread, LastPrice_Ob, LastPrice_Pref = get_last_prices(TOKEN, figi_Ob, figi_Pref)
                             Money =  int(PositionResponse.money[0].units)
-
-                            # У Сбера и Ростелекома лотность 10 шт.
-                            if name_Ob == 'SBER' or name_Ob == 'RTKM':
-                                KolVo = int(Money/LastPrice_Ob/10*0.98)
-                            else:
-                                KolVo = int(Money/LastPrice_Ob*0.98)
-
+                            lot_size = info_Ob.get('lot_size', 'Информация о лотности не найдена')
+                            KolVo = int(Money/LastPrice_Ob/lot_size*0.98)
                             trade (TOKEN, str(uuid.uuid4()), Account_id, KolVo, figi_Ob, OrderDirection.ORDER_DIRECTION_BUY)
                                 
                         print("---------------------------------------------")
 
                     # Спред больше price_2 покупаем префа если ничего нет, или продаем обычку и покупаем префа
                     elif spread > price_2:
+                        
 
                         print ('Спред меньше 1 делаем соотношение 0% в обычке и 100% в префе') 
                         print ('Текущий спред', spread)
                         print ('Цена обычки =', LastPrice_Ob)
                         print ('Цена префов =', LastPrice_Pref)
                         print ('Дата:', datetime.datetime.now()) 
-                        print ('Наш портфель акций:', Securities)
-                        print ('У нас в портфеле денег:', int(PositionResponse.money[0].units), 'руб.')
+                        print('Портфель:')
+                        KPrefa, KObich, Securities, PositionResponse = get_portfolio_info(TOKEN, df)
 
                         KolVo_Ob = 0
                         KolVo_Pref = 0
@@ -382,22 +381,17 @@ if __name__ == '__main__':
                         # Если у нас в портфеле обычка мы ее продаем и покупаем префа
                         if figi_Ob_found > 0:
 
-                            # У Сбера и Ростелекома лотность 10 шт.
-                            if name_Ob == 'SBER' or name_Ob == 'RTKM':
-                                KolVo_Ob = KolVo_Ob/10
-
+                            # Совершаем продажу
                             print('У нас в портфеле обычка', KolVo_Ob, 'шт. нужно ее продать и купить префа')
+                            lot_size = info_Ob.get('lot_size', 'Информация о лотности не найдена')
+                            KolVo_Ob = KolVo_Ob/lot_size
                             trade (TOKEN, str(uuid.uuid4()), Account_id, KolVo_Ob, figi_Ob, OrderDirection.ORDER_DIRECTION_SELL)
 
+                            # Получаем информацию для покупки
                             spread, LastPrice_Ob, LastPrice_Pref = get_last_prices(TOKEN, figi_Ob, figi_Pref)
                             Money =  int(PositionResponse.money[0].units)
-
-                            # У Сбера и Ростелекома лотность 10 шт.
-                            if name_Pref == 'SBERP' or name_Pref == 'RTKMP':
-                                KolVo = int(Money/LastPrice_Ob/10*0.98)
-                            else:
-                                KolVo = int(Money/LastPrice_Ob*0.98)
-
+                            lot_size = figi_Pref.get('lot_size', 'Информация о лотности не найдена')
+                            KolVo = int(Money/LastPrice_Pref/lot_size*0.98)
                             trade (TOKEN, str(uuid.uuid4()), Account_id, KolVo, figi_Pref, OrderDirection.ORDER_DIRECTION_BUY)
 
                         if figi_Pref_found > 0:
@@ -408,15 +402,11 @@ if __name__ == '__main__':
 
                             print(f'В портфеле нет акций {name_Pref}, нужно купить обычку')
 
+                            # Получаем информацию для покупки
                             spread, LastPrice_Ob, LastPrice_Pref = get_last_prices(TOKEN, figi_Ob, figi_Pref)
                             Money =  int(PositionResponse.money[0].units)
-
-                            # У Сбера и Ростелекома лотность 10 шт.
-                            if name_Pref == 'SBERP' or name_Pref == 'RTKMP':
-                                KolVo = int(Money/LastPrice_Pref/10*0.98)
-                            else:
-                                KolVo = int(Money/LastPrice_Pref*0.98)
-
+                            lot_size = figi_Pref.get('lot_size', 'Информация о лотности не найдена')
+                            KolVo = int(Money/LastPrice_Pref/lot_size*0.98)
                             trade (TOKEN, str(uuid.uuid4()), Account_id, KolVo, figi_Pref, OrderDirection.ORDER_DIRECTION_BUY)
 
                         print("---------------------------------------------")
@@ -427,8 +417,8 @@ if __name__ == '__main__':
                         print ('Цена обычки =', LastPrice_Ob)
                         print ('Цена префов =', LastPrice_Pref)
                         print ('Дата:', datetime.datetime.now()) 
-                        print ('Наш портфель акций:', Securities)
-                        print ('У нас в портфеле денег:', int(PositionResponse.money[0].units), 'руб.')
+                        print('Портфель:')
+                        KPrefa, KObich, Securities, PositionResponse = get_portfolio_info(TOKEN, df)
                         print("---------------------------------------------")
                         
                     time.sleep(30)
